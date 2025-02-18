11278128a224d2eb7b0f968e3b85367ac70166b4
keuznub <breixopastoriza@gmail.com>
2025-02-18 19:12:16 +0100

    navigate? build

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, FormEvent, useContext, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link/*useNavigate*/ } from 'react-router-dom'
 import { useAuth } from '../contexts/AuthContext'
 import InputComponent from '../components/InputComponent'
 import ButtonComponent from '../components/ButtonComponent'
